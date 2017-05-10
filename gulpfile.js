7ee0f096577a8bb4af062cce445670fa0d33f142
Willians Paulo <willianspaulo@MacBook-Air-de-Willians.local>
2017-05-10 15:00:54 -0300

    Primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+
+gulp.task('less', function() {
+  return gulp.src('./less/main.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream : true
+    }));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('default', ['less', 'browser-sync'], function() {
+  gulp.watch('./less/**/*.less', ['less', browserSync.reload])
+  gulp.watch('index.html', browserSync.reload)
+});
